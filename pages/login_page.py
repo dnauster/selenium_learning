3973c9b625ca5648744043204562e5d0370bda37
Наумычев Дмитрий Александрович <naumychev_da@frs.local>
2021-11-22 11:41:30 +0300

    six commit.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in self.current_url, "~login~ is not presented in URL"
+        assert 'login' in self.browser.current_url, "~login~ is not presented in URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login FORM link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -10,4 +10,5 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
